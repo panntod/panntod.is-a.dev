a9e2efdcad988d86f68090406f9f8ef05227e897
panntod <munjalindra.pandhu@gmail.com>
2025-02-19 11:15:32 +0700

    feat: Update skills section with new categories and technologies

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,33 +1,63 @@
 export const skills = [
   {
-    title: "Web Development",
+    title: "Frontend Development",
     description: "Building modern web applications with cutting-edge frameworks.",
     items: [
       { name: "Next.js", icon: "icon-[simple-icons--nextdotjs]" },
       { name: "React", icon: "icon-[simple-icons--react]" },
       { name: "Chakra UI", icon: "icon-[simple-icons--chakraui]" },
-      { name: "Tailwind CSS", icon: "icon-[simple-icons--tailwindcss]" }
+      { name: "Tailwind CSS", icon: "icon-[simple-icons--tailwindcss]" },
+      { name: "Material-UI", icon: "icon-[simple-icons--mui]" },
+      { name: "Bootstrap", icon: "icon-[simple-icons--bootstrap]" },
+      { name: "Shadcn UI", icon: "icon-[simple-icons--shadcnui]" }
+    ]
+  },
+  {
+    title: "Backend Development",
+    description: "Developing scalable and efficient backend services.",
+    items: [
+      { name: "NestJS", icon: "icon-[simple-icons--nestjs]" },
+      { name: "Express", icon: "icon-[simple-icons--express]" },
+      { name: "Mongoose", icon: "icon-[simple-icons--mongoose]" },
+      { name: "Prisma", icon: "icon-[simple-icons--prisma]" },
+      { name: "Sequlize", icon: "icon-[simple-icons--sequelize]" }
+    ]
+  },
+  {
+    title: "DevOps",
+    description: "Automating infrastructure, deployment, and scaling with modern DevOps tools.",
+    items: [
+      { name: "Docker", icon: "icon-[simple-icons--docker]" },
+      { name: "Kubernetes", icon: "icon-[simple-icons--kubernetes]" },
+      { name: "AWS", icon: "icon-[simple-icons--amazonwebservices]" },
+      { name: "Google Cloud", icon: "icon-[simple-icons--googlecloud]" },
+      { name: "Vercel", icon: "icon-[simple-icons--vercel]" },
+      { name: "GitHub Actions", icon: "icon-[simple-icons--githubactions]" }
+    ]
+  },
+  {
+    title: "State Management & API",
+    description: "Efficiently managing state and fetching data in modern web applications.",
+    items: [
+      { name: "React Hook Form", icon: "icon-[simple-icons--reacthookform]" },
+      { name: "React Query", icon: "icon-[simple-icons--reactquery]" },
+      { name: "Axios", icon: "icon-[simple-icons--axios]" },
+      { name: "i18n", icon: "icon-[simple-icons--i18next]" }
+    ]
+  },
+  {
+    title: "Public Speaking",
+    description: "Delivering impactful presentations, tech talks, and engaging storytelling.",
+    items: [
+      { name: "Canva", icon: "icon-[simple-icons--canva]" },
+      { name: "PowerPoint", icon: "icon-[simple-icons--microsoftpowerpoint]" },
+      { name: "Google Slides", icon: "icon-[simple-icons--googleslides]" },
+      { name: "Notion", icon: "icon-[simple-icons--notion]" }
     ]
   },
   {
     title: "UI/UX Design",
     description: "Designing intuitive and engaging user experiences.",
     items: [{ name: "Figma", icon: "icon-[simple-icons--figma]" }]
-  },
-  {
-    title: "Backend Development",
-    description: "Developing scalable and efficient backend services.",
-    items: [
-      { name: "Express", icon: "icon-[simple-icons--express]" },
-      { name: "NestJS", icon: "icon-[simple-icons--nestjs]" }
-    ]
-  },
-  {
-    title: "Public Speaking",
-    description: "Delivering impactful presentations and tech talks.",
-    items: [
-      { name: "Canva", icon: "icon-[simple-icons--canva]" },
-      { name: "PowerPoint", icon: "icon-[simple-icons--microsoftpowerpoint]" }
-    ]
   }
 ];
